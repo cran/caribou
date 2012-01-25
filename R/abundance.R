e8a828ad7254e4774220870e348cb4b39b081253
Sophie Baillargeon <Sophie.Baillargeon@mat.ulaval.ca>
2012-01-25 00:00:00 +0000

    version 1.0

--- R/abundance.R
+++ R/abundance.R
@@ -1,64 +1,9 @@
 ###########################################################################
-#   Description:
-#	The function abundance applies the methodology found in Rivest, L.-P.,  
-#	Couturier, S. & Crépeau, H. (1998) for estimating caribou abundance 
-#   using postcalving aggregations detected by radio telemetry
-# 
-#   Usage:
-#	abundance(mat, n, model=c("H","I","T"), B)
-# 
-#   Arguments:
-#
-# 	mat : matrix containning in the first column the number of radio-collared animals 
-#           in the detected (photographed) groups and in the second column the
-#           corresponding size of the detected  groups 
-#  	
-#  	n : total number of active collars during the census 
-#
-#  	model: model to determine the probability that a group with collared animals is detected pi;
-#	    H: homogeneity model pi = r , provides xi>1
-#	    I: independence model pi =  1-(r^xi)	
-# 	    T: threshold model pi = 1 if Xi>=B
-#					    r if  1<=Xi<B
-#
-#  	B : bound for the threshold model 	
-#
-#   Values:
-#
-#	mp : the number of detected groups having radio-collared animals
-#		
-# 	xt : the total number of radio-collared animals found in the detected groups 
-#
-#	gnt : the total number of animals counted in the detected groups
-#
-# 	rr :  estimated parameter related to the probability of detection
-#
-#	se_rr: estimated standard error of rr
-#    
-#	mat_pi: matrix containing xi,gni and pi the detection probability i
-#
-#	T.hat : estimator for the total number of animals in a herd
-# 
-#	se_T.hat : estimated standard error of T.hat
-#
-#	loglikelihood : the maximum value of the loglikelihood function for the detected model
-#
-#	randomness_test: vector with the statistic and the p-value of a score test for the randomness assumption
-#		 available only for the homogeneity model, the independance model and the threshold model for B=2 or 3
-# 
-#   call : function call
-#
-#
-#   Source : Rivest, L.-P., Couturier, S. & Crépeau, H. (1998). Statistical Methods for estimating 
-#            caribou abundance using postcalving aggregations detected by radio telemetry. Biometrics, 54 (3), 865-876
-#
-#
-#  program realized by Hélène Crépeau 
-#  Last update: 21-02-2011
-#
+#  Original program realized by Hélène Crépeau (21-02-2011)
+#  Updates realized by Sophie Baillargeon
 ############################################################################
 
-abundance<-function(mat, n, model=c("H","I","T"), B)
+abundance<-function(mat, n, model=c("H","I","T"), B, maxT.hat)
 { 
   ### Validation des arguments
   mat <- as.matrix(mat)
@@ -121,13 +66,13 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
   ser=sqrt(v2rr)
   
 # 2. Herd size estimation T.hat (equation 4  Rivest et al. 1998)
-  
+
   min_t=sum(gni)
-  max_t=n*max(gni)
+  max_t = if (missing(maxT.hat)) n*max(gni) else maxT.hat
   ft = function(t){t - sum(gni/(pi*(1-(1-gni/t)^n)))}
   abs_ft = function(t){abs(ft(t))}
   T.hat = optimize(abs_ft ,interval=c(min_t,max_t))$minimum
-  T.hat
+  if (round(T.hat)==max_t) warning("'T.hat', the estimated total number of animals, is equal to 'maxT.hat',\nthe upper bound used in the numerical computation of 'T.hat'.\nYou might want to change the value of 'maxT.hat' in the function call.")
   
   
 # 3. Variance estimation of T.hat (equation 6  Rivest et al. 1998)
@@ -234,18 +179,17 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
     pvalue=1-pnorm(zobs)
   }
   if (model=="T") {
-    if (B==2)	{  
+    if (B==2) {  
 # estimate theta 
       r=rr
-      ftheta = function(t)
-      {sum(xi) - 
+      ftheta = function(t) {
+        sum(xi) - 
             sum((exp(exp(t+log(n)+log(gni))) * exp(t+log(n)+log(gni)) - (1 - r) * exp(t+log(n)+log(gni)))/(exp(exp(t+log(n)+log(gni))) - 
                       1 - (1 - r) * exp(t+log(n)+log(gni))))
       } 
       min_theta=-log(max(T.hat-2*se_T.hat,n));
       max_theta=-log(T.hat+2*se_T.hat);
       abs_ftheta = function(t){abs(ftheta(t))}
-#      theta.hat = optimize(abs_ftheta ,interval=c(min_theta,max_theta))$minimum
       theta.hat = optimize(abs_ftheta ,interval=c(min_theta,max_theta))$minimum
       theta.hat
       
@@ -267,7 +211,7 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
       zobs<-sum((xi-eval(gp_tdi))^2-eval(gpp_tdi))/sqrt(vv2)
       pvalue=1-pnorm(zobs)
     }
-    if (B==3) 	{
+    if (B==3)   {
 # estimate theta 
       r=rr
       ftheta = function(t)
@@ -301,11 +245,11 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
       zobs<-sum((xi-eval(gp_tdi))^2-eval(gpp_tdi))/sqrt(vv2)
       pvalue=1-pnorm(zobs)
     }
-    if (B>3)	{
+    if (B>3)  {
       zobs=NA
       pvalue=NA
     }
-  }		
+  }   
   score=c(z_obs=zobs,pvalue=pvalue)
   
   out=list(mp=mp,xt=xt,gnt=gnt,rr=rr,se_rr=ser,mat_pi=mat_pi,T.hat=T.hat,se_T.hat=se_T.hat,loglikelihood=lik_T.hat,randomness_test=score,call=match.call())
@@ -314,13 +258,15 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
   
 }
 
+
 "print.abundance" <- function(x, ...) {
   cat("Given parameters:")
   cat("\nn: Total number of active collars during the census =",eval(x$call$n))
   nmodel <- if(is.null(x$call$model)) "Homogeneity" else { if(x$call$model=="H") "Homogeneity" 
-            else { if(x$call$model=="I") "Independence" else "Threshold" }}
+        else { if(x$call$model=="I") "Independence" else "Threshold" }}
   cat("\nmodel:",nmodel,"model") 
-  if(x$call$model=="T") cat(" with a bound of",x$call$B)
+#  if (!is.null(x$call$model)) { if(x$call$model=="T") cat(" with a bound of",x$call$B) }
+  if (!is.null(x$call$model) && x$call$model=="T") cat(" with a bound of",x$call$B)
   
   cat("\n\nDescriptive statistics:")
   cat("\nmp: Number of detected groups having radio-collared animals =",x$mp)
@@ -341,116 +287,5 @@ abundance<-function(mat, n, model=c("H","I","T"), B)
 }
 
 
-###########################################################################
-#   Description:
-#	The function petersen estimates the total population size based
-#   on the Lincoln-Petersen Index as applied to radio-telemetry data by White and Garrott (1990).
-#	We use the Petersen estimator with Chapman's bias correction and the bias corrected 
-#	standard error estimator of of Seber and Wittes.
-# 
-#	WHITE, G.C., and GARROTT, R.A. 1990. Analysis of wildlife radio-tracking data. San Diego: Academic Press.
-#	
-#
-#   Usage:
-#	petersen(mat,M,S)
-# 
-#   Arguments:
-#
-# 	mat : matrix containing in the first column the number of radio-collared 
-#           in the detected (photographed) groups and in the second column the
-#           corresponding size of the detected  groups 
-#  	
-#  	M : total number of active collars during the census 
-#
-#  	S : the minimum size that define well aggregated group
-#
-#
-#
-#   Values:
-#
-#  	M : total number of active collars during the survey
-#
-#  	S : the minimum size that define well aggregated group
-#
-#	G : Number of well define aggregated groups
-#
-#  	R : total number of radio-collared animal observed in the well aggregated groups  
-
-#  	C : total number of animals observed in the well aggregated groups containing at least one radio-collared animal during the survey
-
-#	LP_T.hat : modified lincoln-petersen estimator for the total number of animals in a herd
-# 
-#	ser_LP : estimated standard error of LP_T.hat
-
-#   mat_aggre : A matrix containing a subset of the input matrix mat: the data for the well aggregated groups only.
-
-#   call : function call
-#
-#
-#  program realized by Hélène Crépeau
-#  Last update: 21-02-2011
-#
-############################################################################
-
-petersen=function(mat,M,S)
-{ 
-  ### Validation des arguments
-  mat <- as.matrix(mat)
-  if (dim(mat)[2]!=2) stop("'mat' must have two columns")
-#  if (any((mat%%1)!=0)||any(mat<0)) stop("'mat' must contain non-negative integers")
-  if (!is.numeric(mat)||any(mat<0)) stop("'mat' must contain non-negative numerics")
-  if (any(mat[,2]<mat[,1])) stop("each number in the second column of 'mat' must be greater or equal to the number in the first column on the same row") 
-  
-  valid.one(M,"numeric")
-  
-  valid.one(S,"numeric")
-  ############
-  
-# gni : vector of the size of detected  groups 
-  gni=mat[,2]
-  
-  mat_aggre=mat[gni>S,]
-  G=dim(mat_aggre)[1]
-  R=sum(mat_aggre[,1])
-  C=sum(mat_aggre[,2])
-  
-  if(M<R) warning("")
-  
-  LP_T.hat=((M+1)*(C+1))/(R+1)-1
-  var_LP=((M+1)*(C+1)*(M-R)*(C-R))/((R+1)^2*(R+2))
-  se_LP=sqrt(var_LP)
-  
-  out<-list(G=G,R=R,C=C,LP_T.hat=LP_T.hat,se_LP=se_LP,mat_aggre=mat_aggre,call=match.call())
-  class(out)<- "petersen"    
-  return(out)
-  
-}
 
 
-"print.petersen" <- function(x, ...) {
-  cat("Given parameters:")
-  cat("\nM: Total number of active collars during the census =",eval(x$call$M))
-  cat("\nS: Minimum size of well define aggregated groups =",eval(x$call$S)) 
-  
-  cat("\n\nDescriptive statistics on well aggregated groups:")
-  cat("\nG: Number of well define aggregated groups =",x$G) 
-  cat("\nR: Total number of radio-collared animal observed in the well aggregated groups =",x$R)
-  cat("\nC: Total number of animals observed in the well aggregated groups =",x$C)
-  
-  cat("\n\nEstimation:\n")
-  tableau3 <- cbind(estimate=round(x$LP_T.hat,0),se=round(x$se_LP,1))
-  rownames(tableau3) <- c("Total number of animals in a herd:")
-  print.default(tableau3, print.gap = 2, quote = FALSE, right=TRUE, ...)
-  
-  cat("\nThis function calculates a Lincoln-Petersen estimator with Chapman's bias correction and the bias corrected standard error estimator of Seber and Wittes\n")
-  
-  invisible(x)
-}
-
-"valid.one" <- function(x,type)
-{
-  if(!eval(call(paste("is.",type,sep=""),x))||length(x)!=1) {
-    error <- paste("'",deparse(substitute(x)),"' must be a length-one object of type ",type,sep="")
-    stop(error)
-  } 
-}
